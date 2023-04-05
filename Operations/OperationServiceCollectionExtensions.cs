f605ed26115e75ccc0c3307b37d0ed6dee37b893
Kyle McClellan <kylemcclellan3@gmail.com>
2023-04-04 21:57:00 -0500

    Operations project

---
+++ Operations/OperationServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace StrawberryShake;
+
+/// <summary>
+/// Extensions of <see cref="IServiceCollection"/> to facilitate generically typed operations.
+/// </summary>
+public static class OperationServiceCollectionExtensions
+{
+}
